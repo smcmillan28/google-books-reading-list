7ea57054fd86a7b1d9d0d79ed5e5535d3bf4b4df
Sammy McMillan <74073409+smcmillan28@users.noreply.github.com>
2021-05-10 23:52:00 -0400

    testing connectivity

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is going to be a Google Books API search/save app.
         </p>
         <a
           className="App-link"
