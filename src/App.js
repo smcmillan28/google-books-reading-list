bd11cbeb1569e23f6950cf0e7bc38ce29feb5e34
Sammy McMillan <74073409+smcmillan28@users.noreply.github.com>
2021-05-12 22:31:15 -0400

    added header with links to different pages/routes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import './App.css';
 import Search from "./pages/Search";
 import Saved from "./pages/Saved";
 import NoMatch from "./pages/NoMatch";
+import Header from "./components/Header";
+import React from "react";
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 
 function App() {
   return (
     <Router basename="/">
+      <Header />
       <Switch>
         <Route exact path="/saved" component={Saved} />
         <Route exact path={["/", "/search"]} component={Search} />
