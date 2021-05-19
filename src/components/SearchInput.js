a4b91bce56060e590f1fe43b57c5b5c00c262149
Sammy McMillan <74073409+smcmillan28@users.noreply.github.com>
2021-05-18 20:53:57 -0400

    edited search page styling

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -1,23 +1,39 @@
 import React from "react";
 import { Container, Row, Col } from "./Grid";
 
+const styles = {
+    formStyles: {
+        margin: "10px", 
+        border: "2px solid black",
+        borderRadius: "5px", 
+        textAlign: "center", 
+        width: "90%"
+    },
+    inputStyles: {
+        width: "100%"
+    }
+}
+
 function SearchInput() {
     return (
         <Container fluid>
-            <Row>
-                <Col size="md-8">
-                    <form className="row g-3">
-                        <div className="col-auto">
+            <Row style={styles.rowStyles}>
+                <Col style={styles.columnStyles} size="md-11">
+                    <form style={styles.formStyles} className="row g-3">
+                        <div className="col-3">
                             <h4>Search for a book</h4>
                         </div>
-                        <div className="col-auto">
-                            <input type="text" className="form-control" placeholder="Enter Book Title" />
+                        <div className="col-6">
+                            <input style={styles.inputStyles} type="text" className="form-control" placeholder="Enter Book Title" />
                         </div>
-                        <div className="col-auto">
-                            <button type="submit" className="btn btn-primary mb-3">Search</button>
+                        <div className="col-1">
+                            <button type="submit" className="btn btn-secondary mb-3">Search</button>
                         </div>
                     </form>
                 </Col>
+            </Row>
+            <Row>
+                <hr></hr>
             </Row>
         </Container>
     )
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,10 +1,28 @@
 import React from "react";
+import Jumbotron from "../components/Jumbotron";
 import SearchInput from "../components/SearchInput";
+import { Col, Row, Container } from "../components/Grid";
 
 function Search() {
-    return(
-        <SearchInput />
+    return (
+        <Container fluid>
+            <Row>
+                <Col size="md-12">
+                    <Jumbotron>
+                        <div>
+                            <h5>Search for your favorite books below, then hit save to add to your library!</h5>
+                        </div>
+                    </Jumbotron>
+                </Col>
+            </Row>
+            <Row>
+                <SearchInput />
+            </Row>
+            <Row>
+                <h2 style={{ textAlign: "center ", textDecoration: "underline "}}>Search Results: </h2>
+            </Row>
+        </Container>
     )
 }
 
-export default Search; 
+export default Search;
